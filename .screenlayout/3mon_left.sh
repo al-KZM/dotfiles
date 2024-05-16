3b96aa4db132e767a98071f34ae275b5619ecae0
Eyal Chocron <x41lakazam@gmail.com>
2024-05-16 12:39:27 +0300

    changes

--- .screenlayout/3mon_left.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x109 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP3-8 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
