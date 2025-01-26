be1856d105d933a1b4564f46ff75545243d57c49
x41lakazam <alakazamjoined@gmail.com>
2025-01-26 11:22:29 +0200

    Add nv functions

--- nvidia/nvinit.sh
+++ nvidia/nvinit.sh
@@ -4,5 +4,5 @@ export NVIDIA_CONFIG_DIR="$CONFIG_DIR/nvidia"
 # This should be first
 source $NVIDIA_CONFIG_DIR/nvenv
 
-
+source $NVIDIA_CONFIG_DIR/functions.sh
 source $NVIDIA_CONFIG_DIR/nvaliases
